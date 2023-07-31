6bb42ddf7b0137d44ffc1ff01f8a7a80be3ac883
crazykim67 <crazykim67@gmail.com>
2023-08-01 08:02:04 +0900

    자동차 스크립트 추가

---
+++ Assets/Scripts/CarScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    private void Update()
+    {
+        float move = speed * Time.deltaTime;
+        transform.Translate(0f, 0f, move);
+    }
+}
---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 특정 위치에서 생성을 담당
+public class Road : MonoBehaviour
+{
+    
+}
